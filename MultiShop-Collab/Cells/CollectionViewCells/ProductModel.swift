acfbd40b4617e6863a55bd8553aae096303195e9
AdityaShinde03 <aryanshinde6039@gmail.com>
2024-06-24 18:59:41 +0530

    shopscreen(Aditya) - Added UI of shop screen and also done with searching logic on basis of product title

---
+++ MultiShop-Collab/Cells/CollectionViewCells/ProductModel.swift
@@ -0,0 +1,16 @@
+//
+//  ProductModel.swift
+//  MultiShop-Collab
+//
+//  Created by webcodegenie on 24/06/24.
+//
+
+import Foundation
+import UIKit
+
+
+struct Product {
+    let title: String
+    let image: String
+    let price: String
+}
